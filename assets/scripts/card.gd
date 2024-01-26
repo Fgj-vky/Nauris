e82a483b05a761c5d384cd67a14abf41786c7354
Lauri Lyytikäinen <lauri.lyytikainen@aalto.fi>
2024-01-26 22:22:26 +0200

    added tweens to cards

--- assets/scripts/card.gd
+++ assets/scripts/card.gd
@@ -2,9 +2,15 @@ extends Node2D
 
 @export var sprite: TextureRect
 
+@export var riseAmount: float
+
+var offsetPos: Vector2
+var originalPos: Vector2
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	originalPos = sprite.position
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,12 +18,19 @@ func _process(delta):
 	pass
 
 func _on_texture_rect_mouse_entered():
-	sprite.modulate = Color(0.65,0.73,0.65,1)
 	sprite.z_index = 1000
-	sprite.position += Vector2(0, -20)
+	var tween = get_tree().create_tween()
+	tween.tween_property(sprite, "position", originalPos - Vector2(0, riseAmount), 0.1)
+	tween.tween_property(sprite, "modulate", Color(0.65,0.73,0.65,1), 0.1)
+	
 
 
 func _on_texture_rect_mouse_exited():
-	sprite.modulate = Color(1,1,1,1)
 	sprite.z_index = 0
-	sprite.position += Vector2(0, 20)
+	var tween = get_tree().create_tween()
+	tween.tween_property(sprite, "position", originalPos, 0.1)
+	tween.tween_property(sprite, "modulate", Color(1,1,1,1), 0.1)
+
+
+func _on_texture_button_pressed():
+	print("card clicked")

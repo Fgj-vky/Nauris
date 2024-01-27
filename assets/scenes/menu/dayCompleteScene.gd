b97e183faf805b4ebc310fd38e985e2c96e8697e
samuelll3d <atte.saari1@gmail.com>
2024-01-27 23:19:25 +0200

    rtx startup option

--- assets/scenes/menu/dayCompleteScene.gd
+++ assets/scenes/menu/dayCompleteScene.gd
@@ -1,9 +1,16 @@
 extends CanvasLayer
 
 
+@onready var global = $"/root/Global" as Globals
+var nextScenePath
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	if global.rtxOn:
+		nextScenePath = "res://assets/scenes/game/rtx_game.tscn"
+	else:
+		nextScenePath = "res://assets/scenes/card_system/hand_demo.tscn"
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,4 +19,4 @@ func _process(delta):
 
 
 func _on_button_pressed():
-	get_tree().change_scene_to_file("res://assets/scenes/card_system/hand_demo.tscn")
+	get_tree().change_scene_to_file(nextScenePath)

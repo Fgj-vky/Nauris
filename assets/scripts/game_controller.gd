a2e1bbaaaca58bfe342f6b0ca6040bac33b0956a
jaakop <jaakko@kronogra.fi>
2024-01-27 21:30:12 +0200

    counting days

--- assets/scripts/game_controller.gd
+++ assets/scripts/game_controller.gd
@@ -1,16 +1,22 @@
 extends Node2D
 
 @export var king: King
-var time = 120.0 # time in seconds left 
+var time = 10.0 # time in seconds left 
 var levelActive = true
 
 var score:int
 @export var scoreMultiplier:int
 
+@onready var global = $"/root/Global" as Globals
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	king.loseCallback = lose
 	score = 0;	
+	if(global.currentDay == null):
+		global.currentDay = 0
+	global.currentDay += 1
+	print("Current day: " + str(global.currentDay))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -33,6 +39,7 @@ func addScore(amout:float):
 
 func win():
 	print("You win")
+	get_tree().reload_current_scene()
 	
 func lose():
 	print("You lose")

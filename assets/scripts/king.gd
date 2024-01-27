c62e5a93a9dd5b53913357289d733899cfd5cf8c
Lauri Lyytikäinen <lauri.lyytikainen@aalto.fi>
2024-01-27 16:53:22 +0200

    added bugs

--- assets/scripts/king.gd
+++ assets/scripts/king.gd
@@ -3,23 +3,30 @@ class_name King
 
 var loseCallback: Callable
 
-var moodMeter = 0.5 # Goes from 0.0 to 1.0
+@onready var kingSprite = $KingSprite
+
+var mood = 0.5 # Goes from 0.0 to 1.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	updateKingSprite()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 	
 func updateMood(moodChange: float):
-	moodMeter += moodChange
+	mood += moodChange
 	moodChange = max(min(moodChange, 1.0), 0.0)
 	
-	if moodMeter <= 0:
+	if mood <= 0:
 		loseCallback.call()
 	
-	print("King mood: " + str(moodMeter))
+	print("King mood: " + str(mood))
 
+func updateKingSprite():
+	# 1 vhappy
+	# 5 vsad(
+	var frame = 5 - (round(4 * mood)) as int
+	kingSprite.frame = frame
+	

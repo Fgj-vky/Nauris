5b0545d5452a3640953a8c740600fe4f3c5fbfda
Lauri Lyytikäinen <lauri.lyytikainen@aalto.fi>
2024-01-27 14:11:04 +0200

    added card table

--- assets/scenes/ui/ui.gd
+++ assets/scenes/ui/ui.gd
@@ -1,14 +1,14 @@
 extends CanvasLayer
 
-@onready var cardHolder = $MarginContainer/Panel/HBoxContainer/Cards
+@onready var cardHolder = $Cards
 
 var slot1: Card
 var slot2: Card
 var slot3: Card
 @onready var hand = $"../Hand" as Hand
-@onready var slotPos1 = $MarginContainer/Panel/HBoxContainer/CardSlot/slotPos1
-@onready var slotPos2 = $MarginContainer/Panel/HBoxContainer/CardSlot2/slotPos2
-@onready var slotPos3 = $MarginContainer/Panel/HBoxContainer/CardSlot3/slotPos3
+@onready var slotPos1 = $MarginContainer/TextureRect/slotPos1
+@onready var slotPos2 = $MarginContainer/TextureRect/slotPos2
+@onready var slotPos3 = $MarginContainer/TextureRect/slotPos3
 
 func addCardToTable(card: Card): 
 	# Add card to next slot if free
@@ -31,18 +31,8 @@ func addCardToTable(card: Card):
 		card.tweenToPos(slotPos3.global_position)
 		card.tweenToRotation(0)
 	if slot1 != null && slot2 != null && slot3 != null:
-		if slot1.resource.cardType != CardResource.CardType.Theme || slot2.resource.cardType != CardResource.CardType.Subject || slot3.resource.cardType != CardResource.CardType.PunchLine:
-				#return
-				pass
-		slot1.queue_free()
-		slot1 = null
-		slot2.queue_free()
-		slot2 = null
-		slot3.queue_free()
-		slot3 = null
-		hand.createRandomCard()
-		hand.createRandomCard()
-		hand.createRandomCard()
+		playCards()
+		
 		
 func checkForSpace():
 	if slot1 == null or slot2 == null or slot3 == null:
@@ -57,3 +47,21 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func removeCards():
+	if slot1.resource.cardType != CardResource.CardType.Theme || slot2.resource.cardType != CardResource.CardType.Subject || slot3.resource.cardType != CardResource.CardType.PunchLine:
+		#return
+		pass
+	slot1.queue_free()
+	slot1 = null
+	slot2.queue_free()
+	slot2 = null
+	slot3.queue_free()
+	slot3 = null
+	hand.createRandomCard()
+	hand.createRandomCard()
+	hand.createRandomCard()
+
+func playCards():
+	await get_tree().create_timer(1).timeout
+	removeCards()

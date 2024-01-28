779929a90896c64db6e3fee38663331435611c1e
Lauri Lyytikäinen <lauri.lyytikainen@aalto.fi>
2024-01-28 13:41:26 +0200

    more spaghetti

--- assets/scenes/event_system/event_system.gd
+++ assets/scenes/event_system/event_system.gd
@@ -14,6 +14,7 @@ var currentEvent: event
 @export var timeEventActive: float
 var eventActive: bool = false
 var eventTimer: float
+@onready var ui: Ui = $"../Ui"
 
 @export var tweenOutAmount: float
 
@@ -42,10 +43,11 @@ func _process(delta):
 
 func spawnEvent():
 	currentEvent = resources.pick_random()
-	sprite.texture = currentEvent.character
+	sprite.texture = currentEvent.character
 	sprite.global_position = sprite.global_position + Vector2(-tweenOutAmount, 0)
 	var tween = get_tree().create_tween()
 	tween.tween_property($'./Sprite2D', "global_position", sprite.global_position + Vector2(tweenOutAmount, 0), 0.5)
+	ui.showEventInfo(currentEvent.name)
 
 func hideEvent():
 	sprite.flip_h = true
@@ -56,6 +58,8 @@ func hideEvent():
 		sprite.global_position = sprite.global_position + Vector2(tweenOutAmount, 0)
 		sprite.flip_hflip_h = false
 		)
+	ui.hideEventInfo()
+		
 	
 
 func dir_contents(path):
